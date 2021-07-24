cb0868a46d50a79028e0a3f84ba249a191638263
sanobar28 <sanobar.mujawar@gmail.com>
2021-07-24 23:46:43 +0530

    [Sanobar]added: address book form validation

---
+++ js/addressBookForm.js
@@ -0,0 +1,45 @@
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('#name-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new Contact()).firstName = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('#address-error');
+    address.addEventListener('input', function () {
+        if (address.value.length == 0) {
+            addressError.textContent = "";
+            return;
+        }
+        try {
+            (new Contact()).address = address.value;
+            addressError.textContent = "";
+        } catch (e) {
+            addressError.textContent = e;
+        }
+    });
+    const phoneNo = document.querySelector('#phone');
+    const phoneNoError = document.querySelector('#phone-error');
+    phoneNo.addEventListener('input', function () {
+        if (phoneNo.value.length == 0) {
+            phoneNoError.textContent = "";
+            return;
+        }
+        try {
+            (new Contact()).phoneNumber = phoneNo.value;
+            phoneNoError.textContent = "";
+        } catch (e) {
+            phoneNoError.textContent = e;
+        }
+    });
+})
---
+++ js/contact.js
@@ -0,0 +1,73 @@
+/**
+ * UC4 Address book contacts with input validations
+ */
+
+class Contact {
+
+    id;
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(firstName) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(firstName)) {
+            this._firstName = firstName;
+        } else {
+            throw "first name is incorrect";
+        }
+    }
+
+    get address() {
+        return this._address;
+    }
+    set address(address) {
+        let nameRegex = RegExp('^[a-zA-Z0-9\\s]{3,}$');
+        let wordArray = address.split(" ");
+        wordArray.forEach(element => {
+            if (!nameRegex.test(element)) {
+                throw "address is incorrect";
+            }
+        });
+        if (nameRegex.test(address)) {
+            this._address = address;
+        } else {
+            throw "address is incorrect";
+        }
+    }
+    
+    get city() {
+        return this._city;
+    }
+    set city(city) {
+        this._city = city;
+    }
+
+    get state() {
+        return this._state;
+    }
+    set state(state) {
+        this._state = state;
+    }
+
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+    set phoneNumber(phoneNumber) {
+        let nameRegex = RegExp('^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$');
+        if (nameRegex.test(phoneNumber)) {
+            this._phoneNumber = phoneNumber;
+        } else {
+            throw "phoneNumber is incorrect";
+        }
+    }
+    get zip() {
+        return this._zip;
+    }
+    set zip(zip) {
+        this._zip = zip;
+    }
+
+    toString() {
+        return "id=" + this.id + ",firstName=" + this.firstName + ",address=" + this.address + ",city=" + this.city + ",state=" + this.state + ",phoneNumber=" + this.phoneNumber + ",zip=" + this.zip;
+    }
+}
